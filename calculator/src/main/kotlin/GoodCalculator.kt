17f74454b06186f94f7a352291695ce1a34a8cc5
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-03-31 15:54:07 -0300

    Sub test

--- calculator/src/main/kotlin/GoodCalculator.kt
+++ calculator/src/main/kotlin/GoodCalculator.kt
@@ -1,13 +1,16 @@
 class GoodCalculator: Calculator {
-    override fun sum(a: String, b: String): String {
-      val (carry, result) = a.zip(b).foldRight(Pair('0', "")) { (leftOperator, rightOperator), (carry, previousValues) ->
-        val (newCarry, result) = sum(leftOperator, rightOperator, carry)
-        Pair(newCarry, result + previousValues)
-      }
+  override fun sum(a: String, b: String): String {
+      val (carry, result) = sumHelper(a, b)
       return if (carry == '1') carry + result else result
     }
 
-    private fun sum(leftOperator: Char, rightOperator: Char, carry: Char): Pair<Char, Char> {
+  private fun sumHelper(a: String, b: String) =
+    a.zip(b).foldRight(Pair('0', "")) { (leftOperator, rightOperator), (carry, previousValues) ->
+      val (newCarry, result) = sum(leftOperator, rightOperator, carry)
+      Pair(newCarry, result + previousValues)
+    }
+
+  private fun sum(leftOperator: Char, rightOperator: Char, carry: Char): Pair<Char, Char> {
         return when (Triple(leftOperator, rightOperator, carry)) {
                 Triple('1', '1', '1') ->   {
                         Pair('1', '1')
@@ -25,38 +28,51 @@ class GoodCalculator: Calculator {
                 Triple('0', '0', '0') ->   {
                         Pair('0', '0')
                     }
-                else -> throw Exception()
+                else -> throw Exception("($leftOperator, $rightOperator, $carry)")
         }
+  }
+  override fun sub(a: String, b: String): String {
+    val sumHelper = sumHelper(a, opposite(b))
+    val second = sumHelper.second
+    val inverseResult = if (sumHelper.first == '1') {
+      sum(second, "0".repeat(second.length-1)+ "1")
+        .dropWhile { it == '0' }
+    } else {
+      opposite(second)
     }
+    return inverseResult
+  }
 
+  private fun opposite(b: String):String {
+    return b.map {
+      if (it == '1') '0' else '1'
+    }.joinToString(
+      separator = ""
+    )
+  }
 
-    override fun sub(a: String, b: String): String {
-        return sum (opposite(a), opposite(b))
-    }
+  override fun mult(multiplicand: String, multiplier: String): String {
+      val valuesToBeSum = multiplicand.fold(listOf<String>()) { allMultValues, multiplicandValue ->
+          val value = multiplier.foldRight("") { c, acc -> acc + mult(multiplicandValue, c) }
+          allMultValues + value
+      }
+      return valuesToBeSum.reduce {left, right -> sum(left, right)}
+  }
 
-    private fun opposite(b: String) = b.map { c -> if (c == '1') '0' else '1' }.toString()
-    override fun mult(multiplicand: String, multiplier: String): String {
-        val valuesToBeSum = multiplicand.fold(listOf<String>()) { allMultValues, multiplicandValue ->
-            val value = multiplier.foldRight("") { c, acc -> acc + mult(multiplicandValue, c) }
-            allMultValues + value
-        }
-        return valuesToBeSum.reduce {left, right -> sum(left, right)}
-    }
+  private fun mult(multiplicand: Char, multiplier: Char): Char {
+      return if (multiplicand == '0' || multiplier == '0') '0'
+      else '1'
+  }
 
-    private fun mult(multiplicand: Char, multiplier: Char): Char {
-        return if (multiplicand == '0' || multiplier == '0') '0'
-        else '1'
-    }
+  override fun div(a: String?, b: String?): String {
+      TODO("Not yet implemented")
+  }
 
-    override fun div(a: String?, b: String?): String {
-        TODO("Not yet implemented")
-    }
+  override fun toHex(binary: String?): String {
+      TODO("Not yet implemented")
+  }
 
-    override fun toHex(binary: String?): String {
-        TODO("Not yet implemented")
-    }
-
-    override fun fromHex(hex: String?): String {
-        TODO("Not yet implemented")
-    }
+  override fun fromHex(hex: String?): String {
+      TODO("Not yet implemented")
+  }
 }
--- calculator/src/test/kotlin/CalculatorTest.kt
+++ calculator/src/test/kotlin/CalculatorTest.kt
@@ -2,15 +2,26 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.io.FileInputStream
 
-class SumTest {
+class CalculatorTest {
 
   @Test
-  fun test() {
+  fun sum() {
     val inputStream = FileInputStream(("src/test/resources/sumTest"))
     val readCsv = ReadValue.readCsv(inputStream)
     inputStream.close()
     readCsv.forEach {
       assertEquals(GoodCalculator().sum(it.left, it.right), it.result)
+    }
+  }
+
+  @Test
+  fun sub() {
+    val inputStream = FileInputStream(("src/test/resources/subTest"))
+    val readCsv = ReadValue.readCsv(inputStream)
+    inputStream.close()
+    readCsv.forEach {
+      val sub = GoodCalculator().sub(it.left, it.right)
+      assertEquals(it.result, sub)
     }
   }
 }
